f769979a97a8b0277ad1ed6f7c8b5d9f65b90dbb
tony matheus <mantony0148@icloud.com>
2020-02-22 18:43:07 -0300

    Implementation of != e testes

--- String/String.h
+++ String/String.h
@@ -10,28 +10,57 @@
 #define String_h
 
 class String {
-  public:
-    String();
-    ~String(void);
-    String(char);
-    String(const char*);
-    String(const String &);
-    int length() const;
-    void operator =(const String&);
-    friend bool operator ==(const String &, const String &);
-  private:
-    char *value;
-    int size;
+public:
+  String();
+  String(char);
+  String(const char*);
+  String(const String&);
+  ~String(void);
+  
+  void operator =(const char*);
+  void operator =(const String&);
+  void operator +(const String&); //not implemented
+  void operator += (const char*); // not implemented
+  void operator += (const String&); // not implemented
+  int length() const;
+  
+  friend bool operator ==(const String &, const String &);
+  friend bool operator !=(const String &, const String &);
+  friend std::ostream & operator<<(std::ostream &, const String &);
+private:
+  char *value;
+  int size;
 };
 
+//String& String::operator+=(const String& str)
+//{
+//  if (this != &str)
+//  {
+//    this->size = str.size;
+//    this->value = new char[this->size];
+//
+//    strcpy_s(this->value, this->size, str.value);
+//  }
+//
+//  return *this;
+//}
+
+//& Is used to get the address
 String::String(){
   value = nullptr;
   size = 0;
 }
 
-String::~String()
+String::~String(void)
 {
   delete[] value;
+  value = nullptr;
+}
+
+String::String(const char word){
+  value = new char[1];
+  value[0] = word;
+  size = 1;
 }
 
 String::String(const String&word){
@@ -42,13 +71,6 @@ String::String(const String&word){
   }
 }
 
-int String::length() const{
-  if(value != nullptr){
-    return size;
-  }else{
-    return 0;
-  }
-}
 
 String::String(const char *word){
   const char * t = word;
@@ -67,13 +89,75 @@ String::String(const char *word){
   }
   size = j;
 }
-
-void String::operator =(const String&word)
-{
-  value = word.value;
-  size = word.size;
+// a = b
+void String::operator =(const String & word){
+  const char * newWord = word.value;
+  this->value = new char[word.size];
+  int j;
+  delete[] word.value;
+  for(j = 0; *newWord; newWord++, j++)
+  {
+    this->value[j] = *newWord;
+  }
+  this->size = j;
 }
 
+//
+void String::operator =(const char* word)
+{
+  const char * newWord = word;
+  int i = 0;
+  while(*word++){
+    i++;
+  }
+  
+  value = new char[i];
+  int j;
+  
+  for(j = 0; *newWord; newWord++, j++)
+  {
+    value[j] = *newWord;
+  }
+  size = j;
+}
+
+void String::operator+=(const char* word){
+  const char * newWord = word;
+  const char * oldWord = this->value;
+  int i = 0;
+  while(*word++){
+    i++;
+  }
+  value = new char[size + i];
+  int j;
+  for(j = 0; *oldWord; oldWord++, j++)
+  {
+    value[j] = *oldWord;
+  }
+  std::cout << value << std::endl;
+  for(j = j; *newWord; newWord++, j++)
+  {
+    value[j] = *newWord;
+  }
+  
+  size = j;
+}
+//void operator += (const String&);
+//void String::operator +=(const String &word){
+//  value = word.value;
+//  size = word.size;
+//}
+
+//length
+int String::length() const{
+  if(value != nullptr){
+    return size;
+  }else{
+    return 0;
+  }
+}
+
+// Operadores Lógicos
 bool operator ==(const String & word, const String & anotherWord){
   if(word.length() == anotherWord.length()){
     for(int i = 0; i < word.length(); i++){
@@ -82,10 +166,40 @@ bool operator ==(const String & word, const String & anotherWord){
       }
     }
   }
+  else
+  {
+    return false;
+  }
+  return true;
+}
+
+bool operator !=(const String & word, const String & anotherWord){
+  if(word.length() != anotherWord.length()){
+    return true;
+  } else {
+    int validate = 0;
+    for(int i = 0; i < word.length(); i++){
+      if(word.value[i] == anotherWord.value[i]) {
+        validate++;
+      }
+    }
+    if(validate > 0){
+      return true;
+    }
+  }
   return false;
+}
+
+std::ostream & operator<<(std::ostream &os, const String &s)
+{
+  for(int i = 0; i < s.size; i++)
+  {
+    os << s.value[i];
+  }
+  return os;
 }
 
 
 
-
 #endif /* String_h */
+
--- String/main.cpp
+++ String/main.cpp
@@ -13,22 +13,145 @@
 using namespace std;
 //* Is used to represent a pointer
 
-//& Is used to get the address
+void testA() {
+  cout << "Starting Test A" << endl;
+  String itemAI;
+  cout << &itemAI << endl;
+  
+  String itemAII = 'b';
+  cout << itemAII << endl;
+  
+  String itemAIIb = "b";
+  cout << itemAIIb << endl;
+  
+  String itemAIII = "Some Kind of test";
+  cout << itemAIII << endl;
+  
+  cout << "Finish Test A" << endl << endl;
+}
 
+void TestB(){
+  cout << "Starting Test B" << endl;
+  String testB;
+  testB = "Test B";
+  
+  String testBI;
+  testBI = testB;
+  cout << "Finish Test B" << endl << endl;
+}
+
+void TestC(){
+  cout << "Starting Test C" << endl;
+  String testC;
+  //  testC = "Test C 1 "+"Test C 2 ";
+  
+  //  String testCI;
+  //  testCI = testC;
+  cout << "Finish Test C" << endl << endl;
+}
+
+
+void TestD(){
+  cout << "Starting Test D" << endl;
+  String testD = "Test D before ";
+    testD += "After";
+  cout << testD << endl;
+//  
+//  String testDI = "Test D 2 before";
+//  testDI += testD;
+  cout << testDI << endl;
+  
+  cout << "Finish Test D" << endl << endl;
+}
+
+void testE(){
+  cout << "Starting Test E" << endl;
+  String size = "tamanho igual a 18";
+  cout << size.length() << endl;
+  cout << "Finish Test E" << endl << endl;
+}
+
+void testF() {
+  cout << "Starting Test F" << endl;
+  String testF = "Testando";
+  
+//  int index = testF.indexOf('T'); // not implemented
+  cout << "Finish Test F" << endl << endl;
+  
+//  cout << index << endl;
+}
+
+void testG() {
+  cout << "Starting Test G" << endl;
+  String uppercase = "grande";
+//  uppercase.upcase(0, uppercase.length() - 1);
+  cout << uppercase << endl;
+  cout << "Finish Test G" << endl << endl;
+}
+
+void testH() {
+  cout << "Starting Test H" << endl;
+  String lowercase = "PEQUENO";
+//  lowercase.downcase(0, lowercase.length() - 1);
+  cout << lowercase << endl;
+  cout << "Finish Test H" << endl << endl;
+}
+
+void testI() {
+  // todo test i
+}
+
+void testJ() {
+  String testJ = "Primeiro é P";
+//  char first = testJ[0];
+//  cout << first << " P " << endl;
+}
+
+void testK(){
+  cout << "item K started" << endl;
+  String test, test2,test3;
+  test = "test";
+  test2 = "test2";
+  test3 = "test";
+  bool ab = test != test2;
+  cout << ab << endl;
+  if( test == test2 ){
+    cout << "not working 1";
+  }else if(test != test2){
+    if(test == test3){
+      cout << "working";
+    }else{
+      cout << "parcially working";
+    }
+  }else{
+    cout << "not working 2";
+  }
+  cout << endl;
+  cout << "item K completed" << endl;
+
+}
 
 int main(int argc, const char * argv[]) {
-  Stack p(32);
-  unsigned  number;
-  cout << "Número positivo? ";
-  cin >> number;
-  do {
-    p.insertElement(number%2);
-    number /= 2;
-  } while( number!=0 );
-  cout << "Binário: ";
-   while( !p.isEmpty() )
-    cout << p.remove();
-  cout << endl;
-  
-  return 0;
+//  String a = "dashduh";
+//  String b;
+//  cout << &a << endl;
+//  cout << &b << endl;
+//  b = a;
+//
+//  cout << &a << endl;
+//  cout << &b << endl;
+  testA();
+  String a;
+  a = "nothing";
+  cout << a << "nothing" << endl;
+  String b = "meu pau te arriba";
+  cout << b <<endl;
+  String c = b;
+  cout << c <<endl;
+  cout << "item A completed" << endl;
+
+  a = "meu piru de asa";
+  a += "b";
+
+  testK();
 }
