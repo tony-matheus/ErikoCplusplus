aa5c5bbb62c4515360eec086208a1f0d9c7e5787
Felipe Strozberg <48066928+FelStroz@users.noreply.github.com>
2020-03-02 12:58:47 -0300

    Update String.h

--- String/String.h
+++ String/String.h
@@ -56,8 +56,6 @@ public:
   friend bool operator <=(const String &, const String &); // a > b
   friend bool operator <=(const String &, const char*); // a > "sadasd"
   friend bool operator <=(const char *, const String &); // "sadasd" > a
-  friend bool operator >=(const String &, const String &);
-  friend bool operator <=(const String &, const String &);
   
   friend std::ostream & operator<<(std::ostream &, const String &);
   friend std::istream & operator>>(std::istream &, const String &);
